69e8137c61b0e5375d5443cf1e4b08299518d94e
acta-non-verba <vikas.joshi@live.com>
2021-11-22 21:29:25 +0530

    Implementation with currentServiceCharter

--- carserveprime.console/Program.cs
+++ carserveprime.console/Program.cs
@@ -1,25 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace carserveprime.console
 {
+    public class Service
+    {
+        public DateTime ServiceAppointment { get; set; }
+        public string CarRegistration { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            DateTime proposedDatetime = scheduleAppointment();
+            List<DateTime> acceptableDateRange = new List<DateTime>
+            {
+               DateTime.Today.AddHours(10), DateTime.Today.AddHours(10).AddMinutes(30),
+               DateTime.Today.AddHours(11), DateTime.Today.AddHours(10).AddMinutes(30),
+               DateTime.Today.AddHours(12), DateTime.Today.AddHours(10).AddMinutes(30),
+               DateTime.Today.AddHours(13), DateTime.Today.AddHours(10).AddMinutes(30),
+               DateTime.Today.AddHours(14),
+               DateTime.Today.AddDays(1).AddHours(10), DateTime.Today.AddDays(1).AddHours(10).AddMinutes(30),
+               DateTime.Today.AddDays(1).AddHours(11), DateTime.Today.AddDays(1).AddHours(11).AddMinutes(30),
+               DateTime.Today.AddDays(1).AddHours(12), DateTime.Today.AddDays(1).AddHours(12).AddMinutes(30),
+               DateTime.Today.AddDays(1).AddHours(13), DateTime.Today.AddDays(1).AddHours(13).AddMinutes(30),
+               DateTime.Today.AddDays(1).AddHours(14),
+               DateTime.Today.AddDays(2).AddHours(10), DateTime.Today.AddDays(2).AddHours(10).AddMinutes(30),
+               DateTime.Today.AddDays(2).AddHours(11), DateTime.Today.AddDays(2).AddHours(11).AddMinutes(30),
+               DateTime.Today.AddDays(2).AddHours(12), DateTime.Today.AddDays(2).AddHours(12).AddMinutes(30),
+               DateTime.Today.AddDays(2).AddHours(13), DateTime.Today.AddDays(2).AddHours(13).AddMinutes(30),
+               DateTime.Today.AddDays(2).AddHours(14),
+               DateTime.Today.AddDays(3).AddHours(10), DateTime.Today.AddDays(3).AddHours(10).AddMinutes(30),
+               DateTime.Today.AddDays(3).AddHours(11), DateTime.Today.AddDays(3).AddHours(11).AddMinutes(30),
+               DateTime.Today.AddDays(3).AddHours(12), DateTime.Today.AddDays(3).AddHours(12).AddMinutes(30),
+               DateTime.Today.AddDays(3).AddHours(13), DateTime.Today.AddDays(3).AddHours(13).AddMinutes(30),
+               DateTime.Today.AddDays(3).AddHours(14),
+               DateTime.Today.AddDays(4).AddHours(10), DateTime.Today.AddDays(4).AddHours(10).AddMinutes(30),
+               DateTime.Today.AddDays(4).AddHours(11), DateTime.Today.AddDays(4).AddHours(11).AddMinutes(30),
+               DateTime.Today.AddDays(4).AddHours(12), DateTime.Today.AddDays(4).AddHours(12).AddMinutes(30),
+               DateTime.Today.AddDays(4).AddHours(13), DateTime.Today.AddDays(4).AddHours(13).AddMinutes(30),
+               DateTime.Today.AddDays(4).AddHours(14)
+            };
+            List<Service> currentServiceCharter = new List<Service>
+            {
+
+               new Service{ ServiceAppointment = acceptableDateRange[0], CarRegistration = "KL08CN7897" }, new Service{ ServiceAppointment = acceptableDateRange[1], CarRegistration = "MP04TY2343" },
+               new Service{ ServiceAppointment = acceptableDateRange[2], CarRegistration = "MP05HU8768" },new  Service{ ServiceAppointment = acceptableDateRange[3], CarRegistration = "MH05YG9088" },
+               new Service{ ServiceAppointment = acceptableDateRange[4], CarRegistration = "DD90FG8900" },
+               new Service{ ServiceAppointment = acceptableDateRange[6], CarRegistration = "KA08CN7897" },new  Service{ ServiceAppointment = acceptableDateRange[7], CarRegistration = "JK08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[9], CarRegistration = "OR08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[10], CarRegistration = "WB08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[12], CarRegistration = "UP08CN7897" },new Service{ ServiceAppointment = acceptableDateRange[13], CarRegistration = "JH08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[15], CarRegistration = "HR08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[16], CarRegistration = "HP08CN7897" },new Service{ ServiceAppointment = acceptableDateRange[17], CarRegistration = "GA08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[18], CarRegistration = "GJ08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[21], CarRegistration = "TS08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[22], CarRegistration = "AR08CN7897" },new Service{ ServiceAppointment = acceptableDateRange[23], CarRegistration = "NL08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[24], CarRegistration = "TR08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[26], CarRegistration = "SK08CN7897" },new Service{ ServiceAppointment = acceptableDateRange[27], CarRegistration = "AN08CN7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[28], CarRegistration = "JH09CN7897" },new Service{ ServiceAppointment = acceptableDateRange[29], CarRegistration = "MP45CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[30], CarRegistration = "BR05CY7897" },new Service{ ServiceAppointment = acceptableDateRange[31], CarRegistration = "HR05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[32], CarRegistration = "HP05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[34], CarRegistration = "GJ05CY7897" },new Service{ ServiceAppointment = acceptableDateRange[35], CarRegistration = "CG05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[37], CarRegistration = "TS05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[38], CarRegistration = "AR05CY7897" },new Service{ ServiceAppointment = acceptableDateRange[39], CarRegistration = "NL05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[40], CarRegistration = "TR05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[42], CarRegistration = "SK05CY7897" },new Service{ ServiceAppointment = acceptableDateRange[43], CarRegistration = "AN05CY7897" },
+               new Service{ ServiceAppointment = acceptableDateRange[44], CarRegistration = "JH05CY7897" }
+            };
+
+            DateTime proposedDatetime = scheduleAppointment(currentServiceCharter);
             Console.WriteLine("Thank you for providing the information, your appointment information can be fixed as below");
             Console.WriteLine(proposedDatetime);
             Console.WriteLine("Press (Y)es or (N)o");
             string finalConfirmation = Console.ReadLine();
             if (finalConfirmation.ToLower() == "y")
             {
-                Console.WriteLine("Appointment is confirmed on "+proposedDatetime);
+                Console.WriteLine("Appointment is confirmed on " + proposedDatetime);
                 //save the appointment                
             }
         }
 
-        private static DateTime scheduleAppointment()
+        private static DateTime scheduleAppointment(List<Service> currentServiceCharter)
         {
             DateTime requestedDatetime;
             Console.WriteLine("Welcome to Car Serve Prime");
@@ -38,7 +101,7 @@ namespace carserveprime.console
             bool isTimeSlotAvailable = false;
             if (isvalidDate == true)
             {
-                isTimeSlotAvailable = checkServicePlan(requestedDatetime);
+                isTimeSlotAvailable = checkCurrentServiceCharter(currentServiceCharter, requestedDatetime);
             }
             DateTime nextAvailableDateTime = default;
             if (isvalidDate == false || isTimeSlotAvailable == false)
@@ -48,15 +111,15 @@ namespace carserveprime.console
             return isvalidDate ? requestedDatetime : nextAvailableDateTime;
         }
 
-        private static bool checkServicePlan(DateTime requestedDatetime)
+        private static bool checkCurrentServiceCharter(List<Service> currentServiceCharter, DateTime requestedDatetime)
         {
-            //Todo: logic for checking the availability
-            return true;
+            return currentServiceCharter.Any(x => x.ServiceAppointment == requestedDatetime);
+
         }
 
         private static DateTime suggestNextAvailableSlot()
         {
-            throw new NotImplementedException();
+            return DateTime.Today.AddDays(12).AddHours(14).AddMinutes(30);
         }
 
         private static DateTime concatDateTime(string date, string time)
