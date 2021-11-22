ffc29f3a22e11e2e760a7561f5911d8f4e6126cb
acta-non-verba <vikas.joshi@live.com>
2021-11-22 16:02:24 +0530

    service appointment without any rules

--- carserveprime.console/Program.cs
+++ carserveprime.console/Program.cs
@@ -6,7 +6,22 @@ namespace carserveprime.console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello to the World of carserverprime!");
-        }
+            Console.WriteLine("Welcome to Carserveprime");
+            Console.WriteLine("If you are here to schedule a service appointment please enter yes(y) else no(n)");
+            string response = Console.ReadLine();
+            if (response.ToLower() != "y")
+            {
+                Console.WriteLine("For any other assistance call toll free number");
+                return;
+            }
+            Console.WriteLine("Can we have your car registration number");
+            string regnNumber = Console.ReadLine();
+            Console.WriteLine("Can we have your preferred date(dd) of appointment for current month");
+            string dateOfAppointment = Console.ReadLine();
+            Console.WriteLine("What time would you prefer between (12 PM-5 PM)");
+            string timeOfServiceDay = Console.ReadLine();
+            DateTime dateOfService=new DateTime(DateTime.Today.Year,DateTime.Today.Month,int.Parse(dateOfAppointment));
+            Console.WriteLine("Thank you for providing the information, your appointment information fixed as below");
+            Console.WriteLine(dateOfService.ToShortDateString()+" "+timeOfServiceDay+" PM");        }
     }
 }
