412199b6d364e9320d789ee232f39ea6e59e481a
acta-non-verba <vikas.joshi@live.com>
2021-11-22 15:05:25 +0530

    initial project structure

---
+++ carserveprime.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace carserveprime.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to the World of carserverprime!");
+        }
+    }
+}
