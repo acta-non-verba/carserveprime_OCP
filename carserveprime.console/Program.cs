9e7b0514382af7b6caebd12866a225d8397ffbce
acta-non-verba <vikas.joshi@live.com>
2021-11-24 16:36:08 +0530

    communicated the appointment to customer

--- carserveprime.console/Program.cs
+++ carserveprime.console/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Net.Mail;
 
 namespace carserveprime.console
 {
     class Program
     {
+        static bool mailSent = false;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Carserveprime");
@@ -20,8 +23,25 @@ namespace carserveprime.console
             string dateOfAppointment = Console.ReadLine();
             Console.WriteLine("What time would you prefer between (12 PM-5 PM)");
             string timeOfServiceDay = Console.ReadLine();
-            DateTime dateOfService=new DateTime(DateTime.Today.Year,DateTime.Today.Month,int.Parse(dateOfAppointment));
+            DateTime dateOfService = new DateTime(DateTime.Today.Year, DateTime.Today.Month, int.Parse(dateOfAppointment));
             Console.WriteLine("Thank you for providing the information, your appointment information fixed as below");
-            Console.WriteLine(dateOfService.ToShortDateString()+" "+timeOfServiceDay+" PM");        }
+            Console.WriteLine(dateOfService.ToShortDateString() + " " + timeOfServiceDay + " PM");
+
+            communicateServiceAppointment(timeOfServiceDay, dateOfService);
+
+        }
+
+        private static void communicateServiceAppointment(string timeOfServiceDay, DateTime dateOfService)
+        {
+            SmtpClient mailClient = new SmtpClient();
+            MailAddress fromEmail = new MailAddress("service@carserveprime.com");
+            MailAddress toEmail = new MailAddress("customer@customerdomain.com");
+            MailMessage message = new MailMessage(fromEmail, toEmail);
+            message.Body = @"Thank you for providing the information, your appointment information fixed as below /n" +
+                            dateOfService.ToShortDateString() + " " + timeOfServiceDay + " PM";
+            message.Subject = "Service apointment at carserverprime";
+            mailClient.Send(message);
+        }
+
     }
 }
